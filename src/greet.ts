7521ddd685735e122d2d7c1b4c51f0a8782bc7f2
Alex Lapinski <contact@alexlapinski.name>
2017-02-05 22:14:21 -0500

    Initial Commit (tutorial for typescript)

---
+++ src/greet.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by alexlapinski on 2/5/17.
+ */
+export function sayHello(name: string) {
+    return `Hello from ${name}!`;
+}
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by alexlapinski on 2/5/17.
+ */
+
+import { sayHello } from './greet';
+
+sayHello("TypeScript");
