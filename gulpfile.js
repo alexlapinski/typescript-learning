b9f75cdae8a5100bf662018fa4fae93534da1499
Alex Lapinski <contact@alexlapinski.name>
2017-02-15 21:56:43 -0500

    Finished uglify part of tutorial

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,34 @@
-var gulp = require("gulp");
-var browserify = require("browserify");
-var source = require("vinyl-source-stream");
-var tsify = require("tsify");
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
 var paths = {
-  pages: ["src/*.html"]
+    pages: ['src/*.html'],
+    typescript: ['src/main.ts']
 };
 
-
-gulp.task("copy-html", function(){
+gulp.task('copy-html', function(){
    return gulp.src(paths.pages)
-       .pipe(gulp.dest("dist"));
+       .pipe(gulp.dest('dist'));
 });
 
-gulp.task("default", ["copy-html"], function(){
+gulp.task('default', ['copy-html'], function(){
    return browserify({
-       baseDir: ".",
+       baseDir: '.',
        debug: true,
-       entries: ["src/main.ts"],
+       entries: paths.typescript,
        cache: {},
        packageCache: {}
    })
        .plugin(tsify)
        .bundle()
-       .pipe(source("bundle.js"))
-       .pipe(gulp.dest("dist"));
+       .pipe(source('bundle.js'))
+       .pipe(buffer())
+       .pipe(sourcemaps.init({loadMaps: true}))
+       .pipe(uglify())
+       .pipe(sourcemaps.write('./'))
+       .pipe(gulp.dest('dist'));
 });
